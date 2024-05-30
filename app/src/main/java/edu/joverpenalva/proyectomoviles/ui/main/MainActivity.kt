e97df5e4b771afcd3e59de9d9946ca9fd4dab31f
Jover7 <117666169+Jover7@users.noreply.github.com>
2024-05-30 19:22:28 +0200

    Vistas

--- app/src/main/java/edu/joverpenalva/proyectomoviles/ui/main/MainActivity.kt
+++ app/src/main/java/edu/joverpenalva/proyectomoviles/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package edu.joverpenalva.proyectomoviles
+package edu.joverpenalva.proyectomoviles.ui.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import edu.joverpenalva.proyectomoviles.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
