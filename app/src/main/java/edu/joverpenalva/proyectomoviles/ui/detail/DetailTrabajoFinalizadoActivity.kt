61d24489a779d48d956d627dda42efde381d1ecf
Sergipks <jover.std@gmail.com>
2024-05-31 14:38:40 +0200

    Vista

--- app/src/main/java/edu/joverpenalva/proyectomoviles/ui/detail/DetailTrabajoFinalizadoActivity.kt
+++ app/src/main/java/edu/joverpenalva/proyectomoviles/ui/detail/DetailTrabajoFinalizadoActivity.kt
@@ -2,17 +2,23 @@ package edu.joverpenalva.proyectomoviles.ui.detail
 
 import android.app.ActivityOptions
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import edu.joverpenalva.proyectomoviles.TrabajadorApplication
 import edu.joverpenalva.proyectomoviles.data.RemoteDataSource
 import edu.joverpenalva.proyectomoviles.data.Repository
 import edu.joverpenalva.proyectomoviles.databinding.ActivityDetailFinalizadaBinding
 import edu.joverpenalva.proyectomoviles.databinding.ActivityDetailPendienteBinding
+import edu.joverpenalva.proyectomoviles.model.trabajos.Result
+import kotlinx.coroutines.launch
 
 class DetailTrabajoFinalizadoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailFinalizadaBinding
+    private lateinit var trabajo: Result
+
     companion object {
         const val TRABAJO_COD = "TRABAJO_COD"
 
@@ -39,6 +45,33 @@ class DetailTrabajoFinalizadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailFinalizadaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        getTrabajo()
+
+        binding.tvCodTrabajo.text = trabajo.codTrabajo
+        binding.tvFecIni.text = trabajo.fechaInicio
+        binding.tvCategoria.text = trabajo.categoria
+        binding.tvDescripcion.text = trabajo.descripcion
+        binding.tvFecFin.text = trabajo.fechaFin
+        binding.tvTiempo.text = trabajo.tiempo.toString()
+
+        if (trabajo.prioridad == 1) {
+            binding.tvPrioridad.setTextColor(Color.RED)
+        } else if (trabajo.prioridad == 2) {
+            binding.tvPrioridad.setTextColor(Color.parseColor("#FFA500"))
+        } else if (trabajo.prioridad == 3) {
+            binding.tvPrioridad.setTextColor(Color.YELLOW)
+        } else {
+            binding.tvPrioridad.setTextColor(Color.GREEN)
+        }
+    }
+
+    private fun getTrabajo() {
+        lifecycleScope.launch {
+            vm.trabajo.collect { t ->
+                trabajo = t
+            }
+        }
     }
 }
